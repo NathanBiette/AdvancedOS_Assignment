4723e0bf6cb9932233b5fd918cbd477b7cf0a176
Nathan Biette <nathan1.biette@free.fr>
2019-04-16 12:16:00 +0800

    Simulate FCFS schedule in progress

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <queue>
 #include "Scheduler.h"
 using namespace std;
 
@@ -41,13 +42,32 @@ void Scheduler::PrintInput(){
 void Scheduler::Simulate(const string &filename){
     ofstream myfile;
     myfile.open(filename, ios::trunc | ios::out);
-    if (myfile.is_open()){
-        for(int i = 0; i < numArrivals; i++){
-        
+    if (myfile.is_open() && arrivals.size() > 0){
+
+        time = 0;
+        job executingJob = {arrivals[0].processId, arrivals[0].burst};
+        myfile << "(" << time << ", " << executingJob.processId << ")\n";
+        int nextArrival = 1;
+        // Iterate over the time steps 
+        while(nextArrival < numArrivals || !readyQ.empty()){
+            if(arrivals[nextArrival].arrivalTime < time + executingJob.remainingTime){
+                time = arrivals[nextArrival].arrivalTime;
+                job newJob = {arrivals[nextArrival].processId, arrivals[nextArrival].burst};
+                readyQ.push(newJob);
+                nextArrival++;
+            }else{
+                time += executingJob.remainingTime;
+                if(!readyQ.empty()){
+                    executingJob = readyQ.pop();
+                    myfile << "(" << time << ", " << executingJob.processId << ")\n";
+                }
+                if(time == arrivals[nextArrival].arrivalTime){
+                    job newJob = {arrivals[nextArrival].processId, arrivals[nextArrival].burst};
+                    readyQ.push(newJob);
+                    nextArrival++;
+                }
+            }
         }
-        myfile << "This is a line.\n";
-        myfile << "This is NOT line.\n";
-        myfile.close();
     }
     else cout << "Unable to open file";
     myfile.close();
--- Scheduler.h
+++ Scheduler.h
@@ -3,26 +3,34 @@
 
 #include <string>
 #include <vector>
+#include <queue>
 
 struct arrival {
   int processId;
   int arrivalTime;
   int burst;
-} ;
+};
+
+struct job {
+  int processId;
+  int remainingTime;
+};
 
 class Scheduler
 {
-     private :
-          //std::string filename;
-          //arrival* arrivals;
-          int numProcesses;
-          int numArrivals;
-          std::vector<arrival> arrivals;
-     public :
-          Scheduler(const std::string&, const int, const int);
-          void ReadInput(const std::string&);
-          void PrintInput();
-          void Simulate(const std::string&);
+    private :
+        //std::string filename;
+        //arrival* arrivals;
+        int time = 0;
+        int numProcesses;
+        int numArrivals;
+        std::vector<arrival> arrivals;
+        std::queue<job> readyQ;
+    public :
+        Scheduler(const std::string&, const int, const int);
+        void ReadInput(const std::string&);
+        void PrintInput();
+        void Simulate(const std::string&);
 };
  
 #endif
